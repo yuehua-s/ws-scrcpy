8b4d4d1977590467d9fd71554ae5bde57abf4fa9
yuehuazhang <yuehuazhang@tencent.com>
2026-01-08 15:49:47 +0800

    feat: WebCodecs 播放器支持 4K 分辨率
    - bounds: 480x480 -> 3840x3840
    - bitrate: 512Kbps -> 8Mbps
    - maxFps: 24 -> 60

--- src/app/player/WebCodecsPlayer.ts
+++ src/app/player/WebCodecsPlayer.ts
@@ -24,10 +24,10 @@ export class WebCodecsPlayer extends BaseCanvasBasedPlayer {
 
     public static readonly preferredVideoSettings: VideoSettings = new VideoSettings({
         lockedVideoOrientation: -1,
-        bitrate: 524288,
-        maxFps: 24,
+        bitrate: 8000000,
+        maxFps: 60,
         iFrameInterval: 5,
-        bounds: new Size(480, 480),
+        bounds: new Size(3840, 3840),
         sendFrameMeta: false,
     });
 
